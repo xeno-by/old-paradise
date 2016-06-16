cd82a45c46d931214cba98bec1bb788ea1ff7301
Eugene Burmako <xeno.by@gmail.com>
2016-06-17 00:45:47 +0200

    updates to the latest version of meta

--- project/build.scala
+++ project/build.scala
@@ -156,7 +156,7 @@ object build extends Build {
   ) settings (
     libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _),
     libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-compiler" % _),
-    libraryDependencies += "org.scalameta" %% "scalameta" % "0.20.0",
+    libraryDependencies += "org.scalameta" %% "scalameta" % "0.23.0",
     libraryDependencies += "org.scalatest" % "scalatest_2.11.0-M3" % "1.9.1b" % "test",
     libraryDependencies += "org.scalacheck" % "scalacheck_2.11" % "1.10.2-SNAPSHOT" % "test",
     scalacOptions += "-Ywarn-unused-import",
