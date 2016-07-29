99daa75a1a7d33bc85dd991e139b30b492c53964
iTakeshi <takeshi.ito.doraemon@gmail.com>
2016-07-29 18:30:12 +0900

    only metaExpandees should be packed into a Term.Block

--- plugin/src/main/scala/org/scalamacros/paradise/typechecker/Expanders.scala
+++ plugin/src/main/scala/org/scalamacros/paradise/typechecker/Expanders.scala
@@ -124,11 +124,11 @@ trait Expanders {
               }
             }
           }
-          val metaArg = (metaTargs ++ metaVargss.flatten ++ metaExpandees) match {
-            case Nil => Nil
+          val metaArgs = metaTargs ++ metaVargss.flatten ++ List(metaExpandees match {
+            case Nil => throw MacroExpansionException
             case tree :: Nil => tree
             case list @ _ :: tail => scala.meta.Term.Block(list.asInstanceOf[Seq[scala.meta.Stat]])
-          }
+          })
 
           val classloader = {
             val m_findMacroClassLoader = analyzer.getClass.getMethods().find(_.getName == "findMacroClassLoader").get
@@ -149,7 +149,7 @@ trait Expanders {
           val metaExpansion = {
             // NOTE: this method is here for correct stacktrace unwrapping
             def macroExpandWithRuntime() = {
-              try newStyleMacroMeth.invoke(annotationModule, metaArg).asInstanceOf[MetaTree]
+              try newStyleMacroMeth.invoke(annotationModule, metaArgs.asInstanceOf[List[AnyRef]].toArray: _*).asInstanceOf[MetaTree]
               catch {
                 case ex: Throwable =>
                   val realex = ReflectionUtils.unwrapThrowable(ex)
