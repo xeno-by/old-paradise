efb42379a3dbb4277c52eef291469e0f8c5ebf82
Dveim <alexandrolgash@gmail.com>
2016-08-17 12:28:43 +0300

    uncomment tests

--- tests/src/main/scala/annotations/new/main/Defs.scala
+++ tests/src/main/scala/annotations/new/main/Defs.scala
@@ -1,36 +1,36 @@
-//package main
-//
-//import scala.annotation.compileTimeOnly
-//import scala.meta._
-//
-//@compileTimeOnly("@printDef not expanded")
-//class printDef extends scala.annotation.StaticAnnotation {
-//  inline def apply(defn: Any) = meta {
-//    assert(defn.is[Defn.Def])
-//    q"println(${defn.toString})"
-//  }
-//}
-//
-//@compileTimeOnly("@printVal not expanded")
-//class printVal extends scala.annotation.StaticAnnotation {
-//  inline def apply(defn: Any) = meta {
-//    assert(defn.is[Defn.Val])
-//    q"println(${defn.toString})"
-//  }
-//}
-//
-//@compileTimeOnly("@printClass not expanded")
-//class printClass extends scala.annotation.StaticAnnotation {
-//  inline def apply(defn: Any) = meta {
-//    assert(defn.is[Defn.Class])
-//    q"println(${defn.toString})"
-//  }
-//}
-//
-//
-//@compileTimeOnly("@identity not expanded")
-//class identity extends scala.annotation.StaticAnnotation {
-//  inline def apply(defn: Defn) = meta {
-//    defn
-//  }
-//}
+package main
+
+import scala.annotation.compileTimeOnly
+import scala.meta._
+
+@compileTimeOnly("@printDef not expanded")
+class printDef extends scala.annotation.StaticAnnotation {
+  inline def apply(defn: Any) = meta {
+    assert(defn.is[Defn.Def])
+    q"println(${defn.toString})"
+  }
+}
+
+@compileTimeOnly("@printVal not expanded")
+class printVal extends scala.annotation.StaticAnnotation {
+  inline def apply(defn: Any) = meta {
+    assert(defn.is[Defn.Val])
+    q"println(${defn.toString})"
+  }
+}
+
+@compileTimeOnly("@printClass not expanded")
+class printClass extends scala.annotation.StaticAnnotation {
+  inline def apply(defn: Any) = meta {
+    assert(defn.is[Defn.Class])
+    q"println(${defn.toString})"
+  }
+}
+
+
+@compileTimeOnly("@identity not expanded")
+class identity extends scala.annotation.StaticAnnotation {
+  inline def apply(defn: Defn) = meta {
+    defn
+  }
+}
